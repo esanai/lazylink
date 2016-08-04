042de98d1bb2c05bbe208e554e87479477644ef0
Sungmyung Lee <leesungmyung@gmail.com>
2016-08-05 00:44:01 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,81 @@
+ObjC.import('AppKit');
+
+function run(argv) {
+  var res = {};
+  getWebLink(res);
+
+  if (res.hasOwnProperty('title') && res.hasOwnProperty('url')) {
+    copyToClipboard(res);
+    pasteFromClipboard();
+
+    return res.title + ' - ' + res.url;
+  }
+
+  return 'Failed!';
+}
+
+function getWebLink(res) {
+  var browsers = [
+    'Safari',
+    'Google Chrome'
+  ];
+  var strBrowser = '';
+  for (var i = 0; i < browsers.length; i++) {
+    if (Application(browsers[i]).running()) {
+      strBrowser = browsers[i];
+    }
+  }
+  if (strBrowser === ''){
+    return false;
+  }
+
+  res.title = applyJsCode(
+    function () {
+      return document.title;
+    },
+    strBrowser
+  );
+  res.url = applyJsCode(
+    function () {
+      return document.URL;
+    },
+    strBrowser
+  );
+
+  return true;
+}
+
+function applyJsCode(fn, strBrowser) {
+  var browser = Application(strBrowser);
+  var jsCode = '(' + fn.toString() + ').apply(null);';
+  var res = '';
+  if (strBrowser === 'Safari') {
+    res = browser.doJavaScript(
+      jsCode, {
+        "in": browser.windows[0].tabs[0]
+      }
+    );
+  }
+  else {
+    res = browser.windows[0].activeTab.execute({
+        "javascript": jsCode
+      });
+  }
+
+  return res;
+}
+
+function copyToClipboard(res) {
+  var pb = $.NSPasteboard.generalPasteboard;
+  var str1 = $.NSString.alloc.initWithUTF8String('<a href="' + res.url + '">' + res.title +'</a>');
+  var str2 = $.NSString.alloc.initWithUTF8String('[' + res.title + '](' + res.url + ')');
+
+  pb.clearContents;
+  pb.setStringForType(str1, $.NSPasteboardTypeHTML);
+  pb.setStringForType(str2, $.NSPasteboardTypeString);
+}
+
+function pasteFromClipboard() {
+  var se = Application('System Events');
+  se.keystroke('v', { using: 'command down' });
+}
